b3d1505de2e2121091a5d49d6d85b60a25a0d9bb
Angelo <jellotinandrade@gmail.com>
2025-01-03 21:19:12 -0800

    recipes.html, styling, and added images directory

--- db/query.sql
+++ db/query.sql
@@ -1,11 +1,31 @@
 -- CREATE TABLE recipes (
 --     id SERIAL PRIMARY KEY,
---     title VARCHAR(255),
---     description TEXT,
---     ingredients TEXT,
---     steps TEXT,
---     category VARCHAR(100),
---     image_url VARCHAR(255),
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+--     title VARCHAR(255) NOT NULL,
+--     description TEXT
 -- );
+
+-- CREATE TABLE ingredients (
+--     id SERIAL PRIMARY KEY,
+--     recipe_id INT REFERENCES recipes(id) ON DELETE CASCADE,
+--     name VARCHAR(255) NOT NULL,
+--     quantity FLOAT,
+--     quantity_fraction VARCHAR(20),
+--     unit VARCHAR(50)
+-- );
+
+-- CREATE TABLE steps (
+--     id SERIAL PRIMARY KEY,
+--     recipe_id INT NOT NULL REFERENCES recipes(id) ON DELETE CASCADE,
+--     step_number INT NOT NULL,
+--     description TEXT NOT NULL
+-- );
+
+-- CREATE TABLE tags (
+--     id SERIAL PRIMARY KEY,
+--     recipe_id INT NOT NULL REFERENCES recipes(id) ON DELETE CASCADE,
+--     tag VARCHAR(50) NOT NULL
+-- );
+
+
+    
 
